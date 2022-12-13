46b2797c1cdeb13c348b1f5c3986d99eb39ea355
Simon Belbeoch <simon.belbeoch@ln1.eu>
2022-12-13 18:44:44 +0100

    chore(day3): add neccessary function for day 3 part 1

--- 2022/javascript/day3.js
+++ 2022/javascript/day3.js
@@ -1,5 +1,21 @@
+const PRIORITY_ORDER = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+const priorityOfLetter = (letter) => PRIORITY_ORDER.indexOf(letter)+1
+const splitIntoSameSizeParts = (stringToSplit, nbParts= 2) => [
+	stringToSplit.slice(0, stringToSplit.length/nbParts),
+	stringToSplit.slice(nbParts, stringToSplit.length/nbParts+nbParts),
+]
+
+const findLetterInCommon = ([firstString, secondString]) =>
+	firstString
+		.split('')
+		.find(letter =>
+			secondString.includes(letter),
+		)
+
 const run = () => {
 	return 157
 }
+
 const runs = [run]
-module.exports = { runs }
+module.exports = { runs, splitIntoSameSizeParts, findLetterInCommon, priorityOfLetter }
--- 2022/javascript/tests/day3.test.js
+++ 2022/javascript/tests/day3.test.js
@@ -1,9 +1,36 @@
-const { runs } = require('../day3')
+const { runs, splitIntoSameSizeParts, findLetterInCommon, priorityOfLetter } = require('../day3')
 const CARACTERISATION_FILE_ENTRY = '../files/day3_test_entry.txt'
 const CARACTERISATION_EXPECTED_OUTPUT_RUN1 = 157
 const [ run ] = runs
 describe('day3', () => {
-    test('caracterisation testing run 1', async () => {
-        expect(await run(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN1)
-    })
+	test('caracterisation testing run 1', async () => {
+		expect(await run(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN1)
+	})
+	describe('#splitIntoSameSizeParts', function () {
+		it('should split into 2 equals parts "toto" and return ["to","to"]', () => {
+			expect(splitIntoSameSizeParts('toto')).toEqual(['to','to'])
+		})
+	})
+	describe('#findLetterInCommon', function () {
+		it('should find o in ["to","do"]', () => {
+			expect(findLetterInCommon(['to', 'do'])).toEqual('o')
+		})
+		it('should find z in ["zjixgcèvz§(è!èic","OUHGBTUVFRYTUJzYH78IU"]', () => {
+			expect(findLetterInCommon(['zjixgcèvz§(è!èic', 'OUHGBTUVFRYTUJzYH78IU'])).toEqual('z')
+		})
+		it('should find undefined in ["ABC","abc"]', () => {
+			expect(findLetterInCommon(['ABC', 'abc'])).toEqual(undefined)
+		})
+	})
+	describe('#priorityOfLetter', function () {
+		it('should return 1 for a', () => {
+			expect(priorityOfLetter('a')).toEqual(1)
+		})
+		it('should return 26 for z', () => {
+			expect(priorityOfLetter('z')).toEqual(26)
+		})
+		it('should return 42 for P', () => {
+			expect(priorityOfLetter('P')).toEqual(42)
+		})
+	})
 })
