029a201eecefba2a07e55ef48adbcd1ead3dc33e
Simon Belbeoch <simon.belbeoch@ln1.eu>
2022-12-13 20:09:46 +0100

    chore(layout) add a js layout to begin each day with a file reader

---
+++ 2022/javascript/layout.js
@@ -0,0 +1,26 @@
+const readline = require('readline')
+const { createReadStream } = require('fs')
+const events = require('events')
+
+const DEFAULT_FILE = '../files/day_entry.txt'
+
+const init = async (filename) => {
+	const lines = []
+	const reader = readline.createInterface({
+		input: createReadStream(filename),
+		crlfDelay: Infinity,
+	})
+
+	reader.on('line', (line) => {
+		lines.push(line)
+	})
+	await events.once(reader, 'close')
+	return lines
+}
+const run =  async (filename = DEFAULT_FILE) => {
+	const lines = await init(filename)
+	return lines
+}
+
+const runs = [run]
+module.exports = { runs }
