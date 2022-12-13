9172a1466bc43a2281e886f5106743ec2ac4dda2
Simon Belbeoch <simon.belbeoch@ln1.eu>
2022-12-13 19:34:09 +0100

    feat(day3/part2) first implementation of part 2 of day 3

--- 2022/javascript/day3.js
+++ 2022/javascript/day3.js
@@ -1,8 +1,10 @@
 const readline = require('readline')
 const { createReadStream } = require('fs')
 const events = require('events')
+
 const DEFAULT_FILE = '../files/day3_entry.txt'
 const PRIORITY_ORDER = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const NUMBER_OF_LINE_BY_ELF = 3
 
 const priorityOfLetter = (letter) => PRIORITY_ORDER.indexOf(letter)+1
 const splitIntoSameSizeParts = (stringToSplit, nbParts= 2) => [
@@ -14,6 +16,12 @@ const findLetterInCommon = ([firstString, secondString]) =>
 	firstString
 		.split('')
 		.find(letter =>
+			secondString.includes(letter),
+		)
+const findAllLettersInCommon = ([firstString, secondString]) =>
+	firstString
+		.split('')
+		.filter(letter =>
 			secondString.includes(letter),
 		)
 
@@ -35,10 +43,36 @@ const init = async (filename) => {
 	return totalPriority
 }
 
+const retrieveOnlyUniqueChars = string => [...new Set(string)].join('')
+const init2 = async (filename) => {
+	const itemsForAGroup = []
+	let totalPriority = 0
+	const reader = readline.createInterface({
+		input: createReadStream(filename),
+		crlfDelay: Infinity,
+	})
+
+	reader.on('line', (line) => {
+		itemsForAGroup.push(retrieveOnlyUniqueChars(line))
+		if(itemsForAGroup.length % NUMBER_OF_LINE_BY_ELF === 0) {
+			const firstLettersInCommon = findAllLettersInCommon([itemsForAGroup.pop(), itemsForAGroup.pop()]).join('')
+			const letter = findLetterInCommon([firstLettersInCommon, itemsForAGroup.pop()])
+			const priority = priorityOfLetter(letter)
+			totalPriority += priority
+		}
+	})
+	await events.once(reader, 'close')
+	return totalPriority
+}
+
 const run =  async (filename = DEFAULT_FILE) => {
 	const totalPriority = await init(filename)
 	return totalPriority
 }
+const run2 =  async (filename = DEFAULT_FILE) => {
+	const totalPriority = await init2(filename)
+	return totalPriority
+}
 
-const runs = [run]
+const runs = [run, run2]
 module.exports = { runs, splitIntoSameSizeParts, findLetterInCommon, priorityOfLetter }
--- 2022/javascript/tests/day3.test.js
+++ 2022/javascript/tests/day3.test.js
@@ -1,10 +1,14 @@
 const { runs, splitIntoSameSizeParts, findLetterInCommon, priorityOfLetter } = require('../day3')
 const CARACTERISATION_FILE_ENTRY = '../files/day3_test_entry.txt'
 const CARACTERISATION_EXPECTED_OUTPUT_RUN1 = 157
-const [ run ] = runs
+const CARACTERISATION_EXPECTED_OUTPUT_RUN2 = 70
+const [ run, run2 ] = runs
 describe('day3', () => {
 	test('caracterisation testing run 1', async () => {
 		expect(await run(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN1)
+	})
+	test('caracterisation testing run 2', async () => {
+		expect(await run2(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN2)
 	})
 	describe('#splitIntoSameSizeParts', function () {
 		it('should split into 2 equals parts "toto" and return ["to","to"]', () => {
