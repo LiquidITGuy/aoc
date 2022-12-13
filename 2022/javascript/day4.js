fdb50d376e305834c453030d29f7ae7272a59065
Simon Belbeoch <simon.belbeoch@ln1.eu>
2022-12-13 20:08:34 +0100

    chore(day4): init day 4

--- 2022/javascript/day4.js
+++ 2022/javascript/day4.js
@@ -2,73 +2,25 @@ const readline = require('readline')
 const { createReadStream } = require('fs')
 const events = require('events')
 
-const DEFAULT_FILE = '../files/day3_entry.txt'
-const PRIORITY_ORDER = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-const NUMBER_OF_LINE_BY_ELF = 3
-
-const priorityOfLetter = (letter) => PRIORITY_ORDER.indexOf(letter)+1
-const splitIntoSameSizeParts = (stringToSplit, nbParts= 2) => [
-	stringToSplit.slice(0, stringToSplit.length/nbParts),
-	stringToSplit.slice(-stringToSplit.length/nbParts),
-]
-
-const findLettersInCommon = (stringsArray) => {
-	const lastString = retrieveOnlyUniqueChars(stringsArray.pop()).split('')
-	return stringsArray.reduce((currentCommonString, stringToEvaluate) =>
-		currentCommonString
-		.filter(char => retrieveOnlyUniqueChars(stringToEvaluate).includes(char)),
-	lastString)
-}
-
+const DEFAULT_FILE = '../files/day4_entry.txt'
 
 const init = async (filename) => {
-	let totalPriority = 0
+	const lines = []
 	const reader = readline.createInterface({
 		input: createReadStream(filename),
 		crlfDelay: Infinity,
 	})
 
 	reader.on('line', (line) => {
-		const compartments = splitIntoSameSizeParts(line)
-		const letter = findLettersInCommon(compartments)[0]
-		const priority = priorityOfLetter(letter)
-		totalPriority+=priority
+		lines.push(line)
 	})
 	await events.once(reader, 'close')
-	return totalPriority
+	return lines
 }
-
-const retrieveOnlyUniqueChars = string => [...new Set(string)].join('')
-const init2 = async (filename) => {
-	let itemsForAGroup = []
-	let totalPriority = 0
-	const reader = readline.createInterface({
-		input: createReadStream(filename),
-		crlfDelay: Infinity,
-	})
-
-	reader.on('line', (line) => {
-		itemsForAGroup.push(retrieveOnlyUniqueChars(line))
-		if(itemsForAGroup.length % NUMBER_OF_LINE_BY_ELF === 0) {
-			const lettersInCommon = findLettersInCommon(itemsForAGroup)
-			const letter = lettersInCommon[0]
-			const priority = priorityOfLetter(letter)
-			totalPriority += priority
-			itemsForAGroup = []
-		}
-	})
-	await events.once(reader, 'close')
-	return totalPriority
-}
-
 const run =  async (filename = DEFAULT_FILE) => {
-	const totalPriority = await init(filename)
-	return totalPriority
-}
-const run2 =  async (filename = DEFAULT_FILE) => {
-	const totalPriority = await init2(filename)
-	return totalPriority
+	const lines = await init(filename)
+	return lines
 }
 
-const runs = [run, run2]
-module.exports = { runs, splitIntoSameSizeParts, priorityOfLetter }
+const runs = [run]
+module.exports = { runs }
--- 2022/javascript/tests/day4.test.js
+++ 2022/javascript/tests/day4.test.js
@@ -1,32 +1,9 @@
-const { runs, splitIntoSameSizeParts, priorityOfLetter } = require('../day3')
-const CARACTERISATION_FILE_ENTRY = '../files/day3_test_entry.txt'
-const CARACTERISATION_EXPECTED_OUTPUT_RUN1 = 157
-const CARACTERISATION_EXPECTED_OUTPUT_RUN2 = 70
-const [ run, run2 ] = runs
-describe('day3', () => {
+const { runs } = require('../day4')
+const CARACTERISATION_FILE_ENTRY = '../files/day4_test_entry.txt'
+const CARACTERISATION_EXPECTED_OUTPUT_RUN1 = 2
+const [ run ] = runs
+describe('day4', () => {
 	test('caracterisation testing run 1', async () => {
 		expect(await run(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN1)
-	})
-	test('caracterisation testing run 2', async () => {
-		expect(await run2(CARACTERISATION_FILE_ENTRY)).toBe(CARACTERISATION_EXPECTED_OUTPUT_RUN2)
-	})
-	describe('#splitIntoSameSizeParts', function () {
-		it('should split into 2 equals parts "toto" and return ["to","to"]', () => {
-			expect(splitIntoSameSizeParts('toto')).toEqual(['to','to'])
-		})
-		it('should split into 2 equals parts "vJrwpWtwJgWrhcsFMMfFFhFp" and return ["vJrwpWtwJgWr","hcsFMMfFFhFp"]', () => {
-			expect(splitIntoSameSizeParts('vJrwpWtwJgWrhcsFMMfFFhFp')).toEqual(['vJrwpWtwJgWr','hcsFMMfFFhFp'])
-		})
-	})
-	describe('#priorityOfLetter', function () {
-		it('should return 1 for a', () => {
-			expect(priorityOfLetter('a')).toEqual(1)
-		})
-		it('should return 26 for z', () => {
-			expect(priorityOfLetter('z')).toEqual(26)
-		})
-		it('should return 42 for P', () => {
-			expect(priorityOfLetter('P')).toEqual(42)
-		})
 	})
 })
